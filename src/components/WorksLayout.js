46a98b6e8202b06903bd551084a921bb7c5b879c
Steff Yang <wonderwallfrank@gmail.com>
2020-12-19 02:13:28 +0800

    Initiate

---
+++ src/components/WorksLayout.js
@@ -0,0 +1,47 @@
+import React from "react"
+import { Link } from "gatsby"
+
+// Styles
+// Background
+// const backgroundDotStyles = {
+//   "z-index": "-1",
+//   "width": "73.4375rem",
+// }
+
+// const backgroundDotItemStyles = {
+//   "bottom": "-21.7rem",
+//   "right": "-29.325rem",
+//   "width": "73.4375rem",
+//   "height": "73.4375rem",
+//   "background": "radial-gradient(50% 50% at 50% 50%,#F9A8D4 0,hsla(0,0%,100%,0) 100%)"
+// }
+
+export default function Layout({ children }) {
+  return (
+    <div className="container mx-auto">
+      {/* <div className="fixed -inset-y-0 right-0 overflow-hidden" style={backgroundDotStyles}>
+        <div className="absolute" style={backgroundDotItemStyles}></div>
+      </div> */}
+      <header className="w-full fixed top-0">
+        <div className="container flex flex-row justify-between items-center py-10">
+          <div>
+          <Link className="tracking-wide text-gray-700 border-b-0 hover:border-b-2 hover:border-pink-600 active:border-b-2 active:border-pink-600 px-0 py-1 font-medium uppercase" to="/">back</Link>
+          </div>
+          <div>
+            <Link className="tracking-wide text-gray-700 border-b-0 hover:border-b-2 hover:border-pink-600 active:border-b-2 active:border-pink-600 px-0 py-1 font-medium uppercase" to="/about/">about</Link>
+          </div>
+        </div>
+      </header>
+      <div className="py-40">
+        {children}
+      </div>
+      <footer className="w-full">
+        <div className="container flex flex-row justify-between items-center py-10">
+          <div>
+            <p className="text-md uppercase">©2020 steff yang | fun and mental are fundamental.</p>
+          </div>
+        </div>
+      </footer>
+    </div>
+  )
+}
---
+++ src/pages/about.js
@@ -0,0 +1,105 @@
+import React from "react"
+import Layout from "../components/Defaultlayout"
+import Headshot from "../assets/headshot.jpg"
+import DivingSteff from "../assets/steff-diving.jpg"
+import JapanSteff from "../assets/steff-japan.jpg"
+import DeerSteff from "../assets/steff-deer.jpg"
+import NewYorkSteff from "../assets/steff-newyork.jpg"
+import KoreaSteff from "../assets/steff-korea.jpg"
+import ElephantSteff from "../assets/steff-elephant.jpg"
+import TainanSteff from "../assets/steff-tainan.jpg"
+
+// markup
+const AboutPage = () => {
+  return (
+    <Layout>
+      <title>About | Steff Yang</title>
+      <div className="p-20">
+        <div className="grid grid-cols-1 md:grid-cols-5 gap-10">
+          <div class="md:col-span-2">
+            <img src={Headshot} alt="Steff" />
+          </div>
+          <div className="md:col-span-3 flex flex-col space-y-20">
+            <div class="pt-20">
+              <p class="text-lg leading-relaxed md:text-xl md:leading-relaxed font-light">
+                Passionate about designing for data democratization and positive behavioral change, I've been solving problems in the marketing technology domain over the past few years: empowering users to make data-driven marketing decisions and ultimately drive tangible business results.
+              </p>
+              <p className="text-lg leading-relaxed md:text-xl md:leading-relaxed font-light">
+                {/* Content to be added */}
+              </p>
+              <p class="text-lg leading-relaxed md:text-xl md:leading-relaxed font-light mt-10">
+                I'm currently venturing into the uncharted territories – healthcare and personal finance – and exploring innovative and impactful solutions in these fields.
+              </p>
+            </div>
+            <div className="">
+              <div class="grid grid-cols-1 md:grid-cols-4 md:gap-2 md:gap-4">
+                <div class="md:col-span-4">
+                  <p class="text-xl uppercase text-pink-400">design experience</p>
+                </div>
+                <div class="mt-4 md:mt-0">
+                  <p class="md:text-lg">2018 – 2020</p>
+                </div>
+                <div class="md:col-span-3">
+                  <p class="md:text-lg">
+                    UNH3O Inc.
+                    <span class="text-gray-400 md:px-4">Taipei, Taiwan</span>
+                  </p>
+                  <p class="md:text-lg text-pink-500">Chief Product Officer / Product Designer</p>
+                </div>
+                <div class="mt-4 md:mt-0">
+                  <p class="md:text-lg">2015 – 2018</p>
+                </div>
+                <div class="md:col-span-3">
+                  <p class="md:text-lg">
+                    QSearch (Dolyman Inc.)
+                    <span class="text-gray-400 px-4">Taipei, Taiwan</span>
+                  </p>
+                  <p class="md:text-lg text-pink-500">UX Engineer</p>
+                </div>
+                <div class="mt-4 md:mt-0">
+                  <p class="md:text-lg">2015 – 2015</p>
+                </div>
+                <div class="md:col-span-3">
+                  <p class="md:text-lg">
+                    Dcard Inc.
+                    <span class="text-gray-400 px-4">Taipei, Taiwan</span>
+                  </p>
+                  <p class="md:text-lg text-pink-500">UX Engineering Summer Intern</p>
+                </div>
+              </div>
+            </div>
+            <div className="mt-20">
+              <p class="text-xl uppercase text-pink-400">you can reach out to me through</p>
+              <p class="text-lg mt-4">steffyangc@gmail.com</p>
+            </div>
+          </div>
+        </div>
+        <div className="hidden md:grid md:grid-cols-3 gap-10 mt-40">
+          <div className="col-span-2">
+            <img src={DivingSteff} alt="Steff diving" />
+          </div>
+          <div>
+            <img src={JapanSteff} alt="Taken in Japan by Steff" />
+          </div>
+          <div>
+            <img src={KoreaSteff} alt="Taken in South Korea by Steff" />
+          </div>
+          <div>
+            <img src={DeerSteff} alt="A deer taken in Japan by Steff" />
+          </div>
+          <div>
+            <img src={ElephantSteff} alt="An elephant taken in Thailand by Steff" />
+          </div>
+          <div>
+            <img src={NewYorkSteff} alt="Taken in New York by Steff" />
+          </div>
+          <div className="col-span-2">
+            <img src={TainanSteff} alt="Steff in Tainan, Taiwan" />
+          </div>
+        </div>
+      </div>
+    </Layout>
+  )
+}
+
+export default AboutPage
